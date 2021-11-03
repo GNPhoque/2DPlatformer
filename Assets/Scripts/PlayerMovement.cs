90e54ab8d3efc2476cfc4e95671bba6cf575da48
Phoque <lafont_jordan@hotmail.fr>
2021-11-03 13:40:03 +0100

    Flip player on movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,13 @@ public class PlayerMovement : MonoBehaviour
 	int remainingJumps;
 	bool pressingJump;
 	bool holdingJump;
+	Transform t;
 	Vector3 direction;
+
+	void Awake()
+	{
+		t = GetComponent<Transform>();
+	}
 
 	void Start()
 	{
@@ -27,16 +33,24 @@ public class PlayerMovement : MonoBehaviour
 
 	void Update()
 	{
+		//GET JUMP INPUT
 		if (Input.GetButtonDown("Jump"))
 		{
 			pressingJump = true;
 		}
 		holdingJump = Input.GetButton("Jump");
+		//GET MOVE INPUT
 		direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+		//FLIP PLAYER SPRITE
+		if (direction.normalized.x != 0)
+		{
+			t.localScale = new Vector3(direction.normalized.x, 1f, 1f);
+		}
 	}
 
 	void FixedUpdate()
 	{
+		//JUMP
 		if (pressingJump)
 		{
 			pressingJump = false;
@@ -46,7 +60,9 @@ public class PlayerMovement : MonoBehaviour
 				remainingJumps--;
 			}
 		}
+		//MOVE
 		rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
+		//BETTER JUMP
 		if (rb.velocity.y < 0 || (rb.velocity.y > 0 && !holdingJump))
 		{
 			rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
