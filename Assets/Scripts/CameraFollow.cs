6bbb49b10523329a4d4d08a5e61c14cdb4b397c5
Phoque <lafont_jordan@hotmail.fr>
2021-11-04 14:24:48 +0100

    Corrected fall animation + secret room + modif camera follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -18,8 +18,13 @@ public class CameraFollow : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		Vector3 newPos = Vector2.Lerp(t.position, playerTransform.position, speed * Time.deltaTime);
-		newPos.z = t.position.z;
-		t.position = newPos;
+		//Vector3 newPos = Vector2.Lerp(t.position, playerTransform.position, speed * Time.deltaTime);
+		//newPos.z = t.position.z;
+		//t.position = newPos;
+		if (playerTransform.position.x < -16 || playerTransform.position.x > 0)
+		{
+			float x = Mathf.Lerp(t.position.x, playerTransform.position.x, speed * Time.deltaTime);
+			t.position = new Vector3(x, t.position.y, t.position.z); 
+		}
 	}
 }
